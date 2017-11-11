4311aa5d984c428c37982131e0b71f6b5daa13e7
rmit-s3530300-amanda-truong <s3530300@student.rmit.edu.au>
2017-11-11 11:33:17 +1100

    Initial commit

---
+++ app/src/main/java/com/struong/fthebus/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.struong.fthebus.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.struong.fthebus.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
